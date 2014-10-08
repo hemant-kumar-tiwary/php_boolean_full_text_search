1780241582d23a1a0d92f69a70b9d2cb44ef71d3
Hemant Tiwari <t.hemantkumar@gmail.com>
2014-10-08 23:43:29 +0530

    modify script to class based and add more option

--- index.php
+++ index.php
@@ -1,77 +1,118 @@
 <?php
 
-/**
- * This function is used to return array in a string format.
+/*
+Author : Hemant Kr Tiwari
+Email : t.hemantkumar@gmail.com
+contact : +91-9818664766
+licence : GNU General Public License
 */
 
- function convertAsString($data = NULL){
-	$str = implode(" ", $data);
-	return $str;
- }
- 
+class GenerateBooleanQueryString{
 
- /**
- * This function is used to set the operator.
- */
-
-function setOperator($temp_stack, &$operator, $temp) {
-	$operator = ((count($temp_stack) > 0) ? $temp : "");
-}
-
-/**
- * This function is used to update the keyword according to boolean operator.
- */
-
-function updateStack(&$temp_stack, $operator) {
-	$temp = array_pop($temp_stack);
-	if($operator == 'and'){
-		$temp = '+'.$temp;	
-	} else if($operator == 'not'){
-		$temp = '-'.$temp;
+	public $boolean_string;
+	/*
+	* default cunstructor to this class
+	*/
+	function __construct($data) {
+		$this->boolean_string = "";
+		$this->getStringForBooleanSearch($data);
 	}
-	array_push($temp_stack,  $temp);	
+
+	
+	/**
+	 * This function is used to return array in a string format.
+	*/
+
+	function convertAsString($data = NULL, $spcialWord= NULL) {
+		$str = "";
+		if(!empty($spcialWord)){
+			foreach($spcialWord as $value)
+				$str .= $value;
+				$str .= " ";
+		}
+		$str .= implode(" ", $data);
+		return addslashes($str);
+	}
+	 
+
+	 /**
+	 * This function is used to set the operator.
+	 */
+
+	function setOperator($temp_stack, &$operator, $temp) {
+		$operator = ((count($temp_stack) > 0) ? $temp : "");
+	}
+
+	/**
+	 * This function is used to update the keyword according to boolean operator.
+	 */
+
+	function updateStack(&$temp_stack, $operator) {
+		$temp = array_pop($temp_stack);
+		if($operator == 'and'){
+			$temp = '+'.$temp;	
+		} else if($operator == 'not'){
+			$temp = '-'.$temp;
+		}
+		array_push($temp_stack,  $temp);	
+	}
+
+	/**
+	 * This function is used to generate boolean search string on the basis of AND OR  NOT operator.
+	*/
+	 
+	function getStringForBooleanSearch($str = "") {
+		
+		$boolean_string = $operator = $temp = "";
+		$str_array = $temp_stack = array();
+		
+		if(empty($str)) 
+			return $boolean_string; 
+		
+		$str = strtolower($str);
+		
+		$tempStr = explode("\"",$str);
+		$replaceWord = array();
+		$replaceWith = array();
+
+		for($i=1;$i<=count($tempStr)-1;$i+=2){
+			$replaceWord[] = '"'.$tempStr[$i].'"';
+			$replaceWith[] = "";
+		}
+		$str = str_replace($replaceWord, $replaceWith,$str);
+		
+		
+		$str_array = explode(" ", $str);
+		$str_array = array_values(array_filter($str_array));
+			
+		do{
+			$temp = array_pop($str_array); 
+			switch($temp){
+				case 'and':
+						$this->setOperator($temp_stack, $operator, $temp);
+						break;
+				case 'or':
+						$this->setOperator($temp_stack, $operator, $temp);
+						break;
+				case 'not':
+						$this->setOperator($temp_stack, $operator, $temp);
+						break;
+				default:
+					$this->updateStack($temp_stack, $operator);
+					array_push($temp_stack,  $temp);
+			}
+		}while(!empty($str_array));
+					
+		if($operator == 'and')
+			$this->updateStack($temp_stack, $operator);
+		
+		$temp_stack = array_reverse($temp_stack);
+		$this->boolean_string = $this->convertAsString($temp_stack, $replaceWord);
+	}
+
 }
 
-/**
- * This function is used to generate boolean search string on the basis of AND OR  NOT operator.
-*/
- 
-function getStringForBooleanSearch($str = "") {
-	//sample data 
-	//$str = "AND AND PHP AND MYSQl OR  JAVA and go AND linux NOT python NOt and Or";
-	
-    $boolean_string = $operator = $temp = "";
-	$str_array = $temp_stack = array();
-	
-	if(empty($str)) 
-		return $boolean_string; 
-	
-	$str = strtolower($str);
-	$str_array = explode(" ", $str);
-	$str_array = array_values(array_filter($str_array));
-		
-	do{
-		$temp = array_pop($str_array); 
-		switch($temp){
-			case 'and':
-					setOperator($temp_stack, $operator, $temp);
-					break;
-			case 'or':
-					setOperator($temp_stack, $operator, $temp);
-					break;
-			case 'not':
-					setOperator($temp_stack, $operator, $temp);
-					break;
-			default:
-				updateStack($temp_stack, $operator);
-				array_push($temp_stack,  $temp);
-		}
-	}while(!empty($str_array));
-				
-	if($operator == 'and')
-		updateStack($temp_stack, $operator);
-	
-	$temp_stack = array_reverse($temp_stack);
-	$boolean_string = convertAsString($temp_stack);
-	return $boolean_string;
-}
+## String that need to convert it into boolean search string that finally used in query
+$data = '"account manager" or python and linux';
+$obj  = new GenerateBooleanQueryString($data);
+echo $obj->boolean_string;
